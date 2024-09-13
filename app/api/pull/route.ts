b2c1a6625158a0f2da24737b5939d8a80172a079
Scott Trinh <scott@scotttrinh.com>
2024-09-13 16:07:14 -0400

    Use generated query

--- app/api/pull/route.ts
+++ app/api/pull/route.ts
@@ -1,53 +1,47 @@
-import { NextRequest } from 'next/server';
-import { client } from '../../../lib/edgedb';
-import { ReplicacheClient, Todo } from '@/dbschema/interfaces';
-import { PullResponse } from 'replicache';
+import { NextRequest } from "next/server";
+import { PatchOperation, PullResponseOKV1 } from "replicache";
+
+import { client } from "@/lib/edgedb";
+import { ReplicacheClient, Todo, helper } from "@/dbschema/interfaces";
+import { getTodosAndClients } from "@/dbschema/queries";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const { clientGroupID } = body;
+  const groupClient = client.withGlobals({
+    currentGroupID: clientGroupID,
+  });
 
   try {
-    return await client.transaction(async tx => {
-      // Fetch all todos related to the client group
-      const todos = await tx.query<Todo>(`
-        SELECT Todo {
-          todoID,
-          content,
-          complete
-        }
-        FILTER .clientGroupID = <str>$clientGroupID
-      `, { clientGroupID });
+    const { todos, clients } = await getTodosAndClients(groupClient);
 
-      // Fetch the last mutation ID for each client in the group
-      const clients = await tx.query<ReplicacheClient>(`
-        SELECT ReplicacheClient { clientID, lastMutationID }
-        FILTER .clientGroupID = <str>$clientGroupID
-      `, { clientGroupID });
-
-      // Construct the patch
-      const patch = [{ op: 'clear' }].concat(todos.map(todo => ({
-        op: 'put',
+    // Construct the patch
+    const patch: PatchOperation[] = [{ op: "clear" } as PatchOperation].concat(
+      todos.map((todo) => ({
+        op: "put",
         key: todo.todoID,
-        value: todo
-      })));
+        value: todo,
+      }))
+    );
 
-      let lastMutationIDChanges: Record<string, number> = {};
-      clients.forEach(client => {
-        lastMutationIDChanges[client.clientID] = client.lastMutationID;
-      });
-
-      // Create the pull response
-      const response = {
-        cookie: Date.now(),  // Current server timestamp as an integer
-        lastMutationIDChanges,
-        patch
-      } as PullResponse;
-
-      return new Response(JSON.stringify(response), { status: 200 });
+    let lastMutationIDChanges: Record<string, number> = {};
+    clients.forEach((client) => {
+      lastMutationIDChanges[client.clientID] = client.lastMutationID;
     });
+
+    // Create the pull response
+    const response = {
+      cookie: Date.now(), // Current server timestamp as an integer
+      lastMutationIDChanges,
+      patch,
+    } satisfies PullResponseOKV1;
+
+    return new Response(JSON.stringify(response), { status: 200 });
   } catch (error) {
-    console.error('Error during pull request:', error);
-    return new Response(null, { status: 500, statusText: 'Failed to process pull request' });
+    console.error("Error during pull request:", error);
+    return new Response(null, {
+      status: 500,
+      statusText: "Failed to process pull request",
+    });
   }
 }
