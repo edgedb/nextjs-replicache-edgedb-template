a764b91f0c786d4bfd2343ffb57ae2b420e4f831
Henrique Doro <opensource@hdoro.dev>
2024-10-21 20:31:27 -0300

    chore: upgrade to Next 15, React 19 & ESLint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+// import pluginReactHooks from 'eslint-plugin-react-hooks'
+// import pluginNext from '@next/eslint-plugin-next'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  // pluginReactHooks.configs.recommended,
+  // pluginNext.configs.recommended,
+]
